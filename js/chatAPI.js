6a59a09880baf8608f110d5c2a4622ce0e870267
blessAchoo <achiujoshua123@gmail.com>
2024-03-21 00:32:41 -0400

    On branch main

--- js/chatAPI.js
+++ js/chatAPI.js
@@ -17,6 +17,12 @@ document.getElementById('generate').addEventListener('click', function() {
         window.img = img;
         localStorage.setItem('imageUrl', imageUrl);
         localStorage.setItem('img', img);
+        /*
+        document.querySelector('body').style.opacity = 0
+        setTimeout(function() { 
+        window.location.href = "./your_world.html"
+        }, 1500)
+        */
         window.location.href="./your_world.html";
         document.dispatchEvent(new CustomEvent('imageReady', { detail: { imageUrl: imageUrl } }));
       }
@@ -26,4 +32,11 @@ document.getElementById('generate').addEventListener('click', function() {
   });
   
   export { imageUrl };
-  
+  /*
+  window.transitionToPage = function(href) {
+    document.querySelector('body').style.opacity = 0
+    setTimeout(function() { 
+        window.location.href = "./your_world.html"
+    }, 500)
+}
+*/
